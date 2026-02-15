d5663d8b9491f2c0376f3d6f320c25a94847a57c
gizzygaming <gizzygamingxbox@gmail.com>
2026-02-15 11:59:39 +0000

    Set base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// If you deploy to GitHub Pages at /REPO_NAME/ set base: "/REPO_NAME/"
 export default defineConfig({
   plugins: [react()],
-  base: "./"
+  base: "/boombox-bingo/",
 });
